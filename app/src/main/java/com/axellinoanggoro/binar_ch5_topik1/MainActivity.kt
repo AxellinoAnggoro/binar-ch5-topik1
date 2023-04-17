dfc500a7d027908dc456ba0ad72fb2dda5010a70
AxellinoAnggoro <axellinoanggoro1311@gmail.com>
2023-04-17 23:13:27 +0700

    add latihan 1-3

--- app/src/main/java/com/axellinoanggoro/binar_ch5_topik1/MainActivity.kt
+++ app/src/main/java/com/axellinoanggoro/binar_ch5_topik1/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.axellinoanggoro.binar_ch5_topik1
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.net.wifi.WifiManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.axellinoanggoro.binar_ch5_topik1.databinding.ActivityMainBinding
 import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     lateinit var wifiManager: WifiManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //show img with glode
         binding.btnShow.setOnClickListener {
             Glide.with(this).load("https://i.ibb.co/zJHYGBP/binarlogo.jpg")
                 .circleCrop()
@@ -25,20 +34,77 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        //change wifi state
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
         binding.btnOn.setOnClickListener {
             wifiManager.isWifiEnabled = true
             Toast.makeText(this, "Wifi enabled", Toast.LENGTH_SHORT).show()
         }
-
         binding.btnOff.setOnClickListener {
             wifiManager.isWifiEnabled = false
             Toast.makeText(this, "Wifi disabled", Toast.LENGTH_SHORT).show()
         }
+
+        //dangerous permission
+        binding.btnLocation.setOnClickListener {
+            val permissionCheck = checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Location DIIZINKAN", Toast.LENGTH_LONG).show()
+                getLongLat()
+            } else {
+                Toast.makeText(this, "Permission Location DITOLAK", Toast.LENGTH_LONG).show()
+                requestLocationPermission()
+            }
+        }
+
+
+
     }
-//    fun enableWifi(view: View) {
-//
-//    }
-//    fun disableWifi(view: View) {
-//
-//    }
+
+    //dangerous permission
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun requestLocationPermission() {
+        requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 201)
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getLongLat() {
+        val locationManager =
+            applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val location: Location? =
+            locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+
+        val latLongText = "Lat: ${location?.latitude} Long : ${location?.longitude}"
+        Log.d("latlong", latLongText)
+        Toast.makeText(this, latLongText, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            201 -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    permissions[0] == android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) {
+                    Toast.makeText(this, "Permissions for Location Permitted", Toast.LENGTH_LONG)
+                        .show()
+                    getLongLat()
+                } else {
+                    Toast.makeText(this, "Permissions for Location Denied", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+            else -> {
+                Toast.makeText(this, "The request code doesn't match", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
 }
